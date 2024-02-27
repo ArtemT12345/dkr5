6f78a4482e81a90700c92b60b790cb0875d604d7
ArtemT12345 <153588244+ArtemT12345@users.noreply.github.com>
2024-02-27 10:30:44 +0300

    Add files via upload

---
+++ dkr51.pas
@@ -0,0 +1,36 @@
+﻿program dkr51;
+const
+  m = 100;
+type
+  TArray = array[1..m] of Integer;
+var
+  arr: Tarray;
+  n, i, j, min, t: Integer;
+procedure SelectionSort(var a: Tarray; n: Integer);
+var
+  i, j, min, t: Integer;
+begin
+  for i := 1 to n - 1 do
+  begin
+    min := i;
+    for j := i + 1 to n do
+    begin
+      if a[j] < a[min] then
+        min := j;
+    end;
+    t := a[min];
+    a[min] := a[i];
+    a[i] := t;
+  end;
+end;
+begin
+  writeln('Количество- ');
+  readln(n);
+  writeln('Введите элемент');
+  for i := 1 to n do
+    read(arr[i]);
+  SelectionSort(arr, n);
+  writeln('Итог:');
+  for i := 1 to n do
+    write(arr[i], ' ');
+end.
---
+++ dkr52.pas
@@ -0,0 +1,54 @@
+﻿program dkr52;
+const
+  m = 100;
+type
+  TArray = array[1..m] of Integer;
+var
+  arr: TArray;
+  n, i: Integer;
+procedure Heapify(var a: TArray; n, i: Integer);
+var
+  z, b, c: Integer;
+begin
+  z := i;
+  b := 2 * i;
+  c := 2 * i + 1;
+  if (b <= n) and (a[b] > a[z]) then
+    z := b;
+  if (c <= n) and (a[c] > a[z]) then
+    z := c;
+  if z <> i then
+  begin 
+    a[i] := a[i] xor a[z];
+    a[z] := a[i] xor a[z];
+    a[i] := a[i] xor a[z];
+    Heapify(a, n, z);
+  end;
+end;
+procedure HeapSort(var a: TArray; n: Integer);
+var
+  i, t: Integer;
+begin
+  for i := n div 2 downto 1 do
+    Heapify(a, n, i);
+  for i := n downto 2 do
+  begin
+    t := a[1];
+    a[1] := a[i];
+    a[i] := t;
+    Heapify(a, i - 1, 1);
+  end;
+end;
+begin
+  writeln('Количество');
+  readln(n);
+  writeln('Ввод');
+  for i := 1 to n do
+7
+3
+read(arr[i]);
+  HeapSort(arr, n);
+  writeln('Итог');
+  for i := 1 to n do
+    write(arr[i]:3);
+end.
